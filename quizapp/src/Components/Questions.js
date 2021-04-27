26edc71fb8a98a93c127af790880bce20a137c27
Subash Lama <iambruce1010@example.com>
2021-04-27 22:29:50 +0545

    shuffle options

--- quizapp/src/Components/Questions.js
+++ quizapp/src/Components/Questions.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Redirect } from "react-router";
 import { Button } from "reactstrap";
 import "../css/questions.css";
+import shuffle from 'shuffle-array';
 
 export default class Questions extends Component {
   constructor(props) {
@@ -16,93 +17,93 @@ export default class Questions extends Component {
       questions: [
         {
           questionText: "What is the capital of France?",
-          answerOptions: [
+          answerOptions: shuffle([
             { answerText: "New York", isCorrect: false },
             { answerText: "London", isCorrect: false },
             { answerText: "Paris", isCorrect: true },
             { answerText: "Dublin", isCorrect: false },
-          ],
+          ]),
         },
         {
           questionText: "Who is CEO of Tesla?",
-          answerOptions: [
+          answerOptions: shuffle([
             { answerText: "Jeff Bezos", isCorrect: false },
             { answerText: "Elon Musk", isCorrect: true },
             { answerText: "Bill Gates", isCorrect: false },
             { answerText: "Tony Stark", isCorrect: false },
-          ],
+          ]),
         },
         {
           questionText: "The iPhone was created by which company?",
-          answerOptions: [
+          answerOptions: shuffle([
             { answerText: "Apple", isCorrect: true },
             { answerText: "Intel", isCorrect: false },
             { answerText: "Amazon", isCorrect: false },
             { answerText: "Microsoft", isCorrect: false },
-          ],
+          ]),
         },
         {
           questionText: "How many Harry Potter books are there?",
-          answerOptions: [
+          answerOptions: shuffle([
             { answerText: "1", isCorrect: false },
             { answerText: "4", isCorrect: false },
             { answerText: "6", isCorrect: false },
             { answerText: "7", isCorrect: true },
-          ],
+          ]),
         },
         {
           questionText: "Blood is filtered by which pair of organs?",
-          answerOptions: [
+          answerOptions: shuffle([
             { answerText: "Heart", isCorrect: false },
             { answerText: "Kidneys", isCorrect: true },
             { answerText: "Liver", isCorrect: false },
             { answerText: "Pancreas", isCorrect: false },
-          ],
+          ]),
         },
         {
           questionText: "Who developed the theory of relativity?",
-          answerOptions: [
+          answerOptions: shuffle([
             { answerText: "Nikola Tesla", isCorrect: false },
             { answerText: "Albert Einstein", isCorrect: true },
             { answerText: "Thomas Edision", isCorrect: false },
             { answerText: "Issac Newton", isCorrect: false },
-          ],
+          ]),
         },
         {
           questionText: "In which country is Mount Everest?",
-          answerOptions: [
+          answerOptions: shuffle([
             { answerText: "Nepal", isCorrect: true },
             { answerText: "Canada", isCorrect: false },
             { answerText: "Japan", isCorrect: false },
             { answerText: "Norway", isCorrect: false },
-          ],
+          ]),
         },
         {
           questionText: "Which is the largest bird alive?",
-          answerOptions: [
+          answerOptions: shuffle([
             { answerText: "Kiwi", isCorrect: false },
             { answerText: "Penguin", isCorrect: false },
             { answerText: "Ostrich", isCorrect: true },
             { answerText: "Sparrow", isCorrect: false },
-          ],
+          ]),
         },
         {
           questionText: "Who discovered radium?",
-          answerOptions: [
+          answerOptions: shuffle([
             { answerText: "The Curies", isCorrect: true },
             { answerText: "Alan Turing", isCorrect: false },
             { answerText: "J.R. Oppenheimer", isCorrect: false },
             { answerText: "Lucas Changretta", isCorrect: false },
-          ],
+          ]),
         },
         {
           questionText: "How many teeth does a horse have?",
-          answerOptions: [
+          answerOptions: shuffle([
             { answerText: "32", isCorrect: false },
             { answerText: "40", isCorrect: true },
             { answerText: "28", isCorrect: false },
             { answerText: "16", isCorrect: false },
-          ],
+          ]),
         },
       ],
     };
